fa06227ebd2b43718b7bcc1d1170010c7282bff3
Fawad <fawadbinasim@ucp.edu.pk>
2018-11-25 22:23:00 +0500

    Assignment - 2 DONE

---
+++ index.php
@@ -0,0 +1,217 @@
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<title>UCP</title>
+	<meta charset="UTF-8">
+	<meta name="description" content="UCP Section H Assignment 2">
+	<meta name="keywords" content="HTML,CSS,XML,JavaScript">
+	<meta name="author" content="M Talha">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="icon" type="image/png" href="images/favicon.ico"/>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap4.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<script src="js/main.js"></script> 
+	</head>
+
+<body background="images/bg.jpg">
+<div class="row jumbotron ">
+    <div class="col-4" style="float: left">
+            <a>UCP JoharTown,Lahore|Contact Us: </a>
+            <a style="color: orange">+(92) 90078601</a>
+    </div>
+        <div class="col-8" style="text-align:right ;">
+                <a>Our Team| FAQ</a>
+                <a href="https://www.facebook.com/fawad.binasim"> <i class="fab fa-facebook-f"></i></a>
+                <a href="https://twitter.com/login"><i class="fab fa-twitter"></i></a>
+                <a href="https://www.instagram.com/fawadbinasim7/"><i class="fab fa-instagram"></i></a>
+                <a> <i class="fab fa-snapchat"></i>
+                    <i class="fab fa-google"></i>
+                    <i class="fab fa-google-drive"></i></a>
+        </div>
+</div>
+
+<div class="row">
+<div class="logo col-4">
+    <img src="images/UcpBlackLogo.png" width="350px">
+</div>
+
+<div class="menuclr col-8">
+
+    <ul>
+        <li><div class="dropdown">
+                <button class="dropbtn">Elements</button>
+                <div class="dropdown-content">
+                    <a href="/">L-1</a>
+                    <a href="/">L-2</a>
+                    <a href="/">L-3</a>
+                </div>
+            </div></li>
+        <li><a href="/">Shop</a></li>
+        <li><a href="/">Blog</a></li>
+        <li><a href="/">Pages</a></li>
+        <li><a href="/">Portfolio</a></li>
+        <li><a href="index.php">Home</a></li>
+    </ul>
+</div>
+</div>
+
+
+<!--<div class="Top-Row">
+       <div id="Strip">
+           <div class="Text">
+              <a>UCP JoharTown,Lahore|Contact Us: </a>
+
+               <div class="links">
+
+                   <a>Our Team| FAQ
+                       <a href="https://www.facebook.com/fawad.binasim"> <i class="fab fa-facebook-f"></i></a>
+                       <a href="https://twitter.com/login"><i class="fab fa-twitter"></i></a>
+                      <a href="https://www.instagram.com/fawadbinasim7/"><i class="fab fa-instagram"></i></a>
+                      <a> <i class="fab fa-snapchat"></i>
+                       <i class="fab fa-google"></i>
+                       <i class="fab fa-google-drive"></i></a>
+
+
+               </div>
+           </div>
+       </div>
+<div class="menuclr">
+        <ul>
+            <li><a href="/">Elements</a></li>
+            <li><a href="/">Shop</a></li>
+            <li><a href="/">Blog</a></li>
+            <li><a href="/">Pages</a></li>
+            <li><a href="/">Portfolio</a></li>
+            <li><a href="index.php">Home</a></li>
+        </ul>
+        <div class="logo">
+            <img src="images/UcpBlackLogo.png" width="350px">
+        </div>
+
+
+    </div>-->
+
+    <h1 class="Hi">Hi,UCP</h1>
+
+
+
+
+    <div style="height:1500px;background-color:black; margin-top:500px ;font-size:15px ">
+        <h1 class="Banner col-sm-12 col-md-12">What I Can Do</h1>
+
+
+
+        <div class="row">
+            <div class="col-md-4" style="background-color:black; color:white">
+                <h2>Fully Responsive</h2>
+                <p>Responsive design is an approach to web page creation that makes use of flexible layouts, flexible images and cascading style sheet media queries.
+                    The goal of responsive design is to build web pages that detect the visitor's screen size and orientation and change the layout accordingly.</p>
+            </div>
+            <div class="col-md-4" style="background-color:black; color:white">
+                <h2>Powerful Portfolios</h2>
+                <p>A portfolio usually represents a portable showcase of your talents.
+                    Today actual portfolios are used less than they used to be by artists, since most commercial artists have a Web site dedicated to showing off their art. But portfolio in its other common meaning is extremely common.</p>
+            </div>
+            <div class="col-md-4" style="background-color:black; color:white">
+                <h2>Shortcodes</h2>
+                <p>A shortcode is a WordPress-specific code that lets you do nifty things with very little effort.
+                    Shortcodes can embed files or create objects that would normally require lots of complicated, ugly code in just one line.</p>
+            </div>
+        </div>
+
+<div class="row">
+        <div class="col-4" style="float: left">
+            <img src="images/profile.png">
+        </div>
+        <div class="col-8" style="text-align: center; color: white;">
+            <h2><b> A Little About My Self</b></h2>
+            <p>
+                I love Cricket, Gaming, Gymoholic,outdoor activities and want to become a good developer
+            </p>
+        </div>
+    </div>
+        <form name="RegForm" action="output.php" onsubmit="return formvalidatefunc()" method="post">
+            <div class="form-group row">
+                <label class="col-sm-2 col-lg-2 col-form-label">Name</label>
+                <div class="col-sm-10 col-lg-2 ">
+                    <input type="text" size=65 name="Name" class="form-control">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label class="col-sm-2 col-lg-2 col-form-label">E-mail</label>
+                <div class="col-sm-10 col-lg-2">
+                    <input type="text" size=65 name="EMail" class="form-control">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label class="col-sm-2 col-lg-2 col-form-label">Password</label>
+                <div class="col-sm-10 col-lg-2">
+                    <input type="password" size=65 name="Password" class="form-control">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label class="col-sm-2 col-lg-2 col-form-label">Address</label>
+                <div class="col-sm-10 col-lg-2">
+                    <input type="text" size=65 name="Address" class="form-control">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label class="col-sm-2 col-lg-2 col-form-label">Telephone</label>
+                <div class="col-sm-10 col-lg-2">
+                    <input type="text" size=65 name="Telephone" class="form-control">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label class="col-sm-2 col-lg-2 col-form-label">Course</label>
+                <div class="col-sm-10 col-lg-2">
+                    <select class="form-control" name="Subject">
+                        <option value="">None</option>
+                        <option value="bscs">BSCS</option>
+                        <option value="bba">BBA</option>
+                        <option value="bca">BCA</option>
+                    </select>
+                </div>
+            </div>
+            <div class="form-group row">
+                <label class="col-sm-2 col-lg-2 col-form-label">Comments</label>
+                <div class="col-sm-10 col-lg-2">
+                    <textarea cols="55" name="Comment" class="form-control"></textarea>
+                </div>
+            </div>
+            <div class="form-group row">
+                <div class="col-sm-2 col-lg-2">
+                    <button type="submit" name="Submit" class="btn btn-primary" onclick="thanks()">Send</button>
+                </div>
+                <div class="col-sm-2 col-lg-2">
+                    <button type="reset" name="Reset" class="btn btn-secondary">Reset</button>
+                </div>
+            </div>
+
+            </p>
+        </form>
+
+
+
+
+
+    </div>
+
+</header>
+    <div class="row jumbotron ">
+        <div class="col-4" style="float: left">
+            <a>UCP JoharTown,Lahore|Contact Us: </a>
+            <a style="color: orange">+(92) 90078601</a>
+        </div>
+        <div class="col-8" style="text-align:right ;">
+            <a>Our Team| FAQ</a>
+            <a href="https://www.facebook.com/fawad.binasim"> <i class="fab fa-facebook-f"></i></a>
+            <a href="https://twitter.com/login"><i class="fab fa-twitter"></i></a>
+            <a href="https://www.instagram.com/fawadbinasim7/"><i class="fab fa-instagram"></i></a>
+            <a> <i class="fab fa-snapchat"></i>
+                <i class="fab fa-google"></i>
+                <i class="fab fa-google-drive"></i></a>
+        </div>
+    </div>
+</body>
+</html>
---
+++ output.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title>UCP</title>
+    <meta charset="UTF-8">
+    <meta name="description" content="UCP Section H Assignment 2">
+    <meta name="keywords" content="HTML,CSS,XML,JavaScript">
+    <meta name="author" content="M Talha">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="images/favicon.ico"/>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap4.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <script src="js/main.js"></script>
+</head>
+
+<body background="images/bg.jpg">
+<div class="row jumbotron ">
+    <div class="col-4" style="float: left">
+        <a>UCP JoharTown,Lahore|Contact Us: </a>
+        <a style="color: orange">+(92) 90078601</a>
+    </div>
+    <div class="col-8" style="text-align:right ;">
+        <a>Our Team| FAQ</a>
+        <a href="https://www.facebook.com/fawad.binasim"> <i class="fab fa-facebook-f"></i></a>
+        <a href="https://twitter.com/login"><i class="fab fa-twitter"></i></a>
+        <a href="https://www.instagram.com/fawadbinasim7/"><i class="fab fa-instagram"></i></a>
+        <a> <i class="fab fa-snapchat"></i>
+            <i class="fab fa-google"></i>
+            <i class="fab fa-google-drive"></i></a>
+    </div>
+</div>
+
+<div class="row">
+    <div class="logo col-4">
+        <img src="images/UcpBlackLogo.png" width="350px">
+    </div>
+
+    <div class="menuclr col-8">
+
+        <ul>
+            <li><div class="dropdown">
+                    <button class="dropbtn">Elements</button>
+                    <div class="dropdown-content">
+                        <a href="/">L-1</a>
+                        <a href="/">L-2</a>
+                        <a href="/">L-3</a>
+                    </div>
+                </div></li>
+            <li><a href="/">Shop</a></li>
+            <li><a href="/">Blog</a></li>
+            <li><a href="/">Pages</a></li>
+            <li><a href="/">Portfolio</a></li>
+            <li><a href="index.php">Home</a></li>
+        </ul>
+    </div>
+</div>
+
+
+<!--<div class="Top-Row">
+       <div id="Strip">
+           <div class="Text">
+              <a>UCP JoharTown,Lahore|Contact Us: </a>
+
+               <div class="links">
+
+                   <a>Our Team| FAQ
+                       <a href="https://www.facebook.com/fawad.binasim"> <i class="fab fa-facebook-f"></i></a>
+                       <a href="https://twitter.com/login"><i class="fab fa-twitter"></i></a>
+                      <a href="https://www.instagram.com/fawadbinasim7/"><i class="fab fa-instagram"></i></a>
+                      <a> <i class="fab fa-snapchat"></i>
+                       <i class="fab fa-google"></i>
+                       <i class="fab fa-google-drive"></i></a>
+
+
+               </div>
+           </div>
+       </div>
+<div class="menuclr">
+        <ul>
+            <li><a href="/">Elements</a></li>
+            <li><a href="/">Shop</a></li>
+            <li><a href="/">Blog</a></li>
+            <li><a href="/">Pages</a></li>
+            <li><a href="/">Portfolio</a></li>
+            <li><a href="index.php">Home</a></li>
+        </ul>
+        <div class="logo">
+            <img src="images/UcpBlackLogo.png" width="350px">
+        </div>
+
+
+    </div>-->
+
+<h1 class="Hi">Hi,UCP</h1>
+
+
+
+
+<div style="height:1500px;background-color:black; margin-top:500px ;font-size:15px ">
+    <h1 class="Banner col-sm-12 col-md-12">What I Can Do</h1>
+
+
+
+    <div class="row">
+        <div class="col-md-4" style="background-color:black; color:white">
+            <h2>Fully Responsive</h2>
+            <p>Responsive design is an approach to web page creation that makes use of flexible layouts, flexible images and cascading style sheet media queries.
+                The goal of responsive design is to build web pages that detect the visitor's screen size and orientation and change the layout accordingly.</p>
+        </div>
+        <div class="col-md-4" style="background-color:black; color:white">
+            <h2>Powerful Portfolios</h2>
+            <p>A portfolio usually represents a portable showcase of your talents.
+                Today actual portfolios are used less than they used to be by artists, since most commercial artists have a Web site dedicated to showing off their art. But portfolio in its other common meaning is extremely common.</p>
+        </div>
+        <div class="col-md-4" style="background-color:black; color:white">
+            <h2>Shortcodes</h2>
+            <p>A shortcode is a WordPress-specific code that lets you do nifty things with very little effort.
+                Shortcodes can embed files or create objects that would normally require lots of complicated, ugly code in just one line.</p>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-4" style="float: left">
+            <img src="images/profile.png">
+        </div>
+        <div class="col-8" style="text-align: center; color: white;">
+            <h2><b> A Little About My Self</b></h2>
+            <p>
+                I love Cricket, Gaming, Gymoholic,outdoor activities and want to become a good developer
+            </p>
+        </div>
+    </div>
+
+
+
+
+
+</div>
+
+
+
+
+
+</header>
+<div class="row jumbotron ">
+    <div class="col-4" style="float: left">
+        <a>UCP JoharTown,Lahore|Contact Us: </a>
+        <a style="color: orange">+(92) 90078601</a>
+    </div>
+    <div class="col-8" style="text-align:right ;">
+        <a>Our Team| FAQ</a>
+        <a href="https://www.facebook.com/fawad.binasim"> <i class="fab fa-facebook-f"></i></a>
+        <a href="https://twitter.com/login"><i class="fab fa-twitter"></i></a>
+        <a href="https://www.instagram.com/fawadbinasim7/"><i class="fab fa-instagram"></i></a>
+        <a> <i class="fab fa-snapchat"></i>
+            <i class="fab fa-google"></i>
+            <i class="fab fa-google-drive"></i></a>
+    </div>
+</div>
+<!--<div id="Strip">
+</div>-->
+</body>
+</html>
